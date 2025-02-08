69491c4f11a644eea4ca37df6045dd0b88c0415a
Sufi065 <132118766+Sufi065@users.noreply.github.com>
2025-02-08 10:57:39 +0530

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+
+void replace(char input[]){
+    int l;
+    l=strlen(input);
+    if(l==0)
+    return;
+    if(input[0]=='x')
+    {
+        for(int i=1;i>0;i--)
+          input[i+2]=input[i];
+        input[0]='a';
+        input[1]='a';
+        input[2]='m';
+        replace(input+3);
+    }
+    else
+    replace(input+1);
+}
+
+int main()
+  {
+    char input[100];
+    cout<<"Enter the string"<<endl;
+    cin>>input;
+    replace(input);
+    cout<<input;
+    return 0;
+}
